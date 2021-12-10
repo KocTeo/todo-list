8b3cf8fa07aee11489aafc6d1731e0cc79749a1c
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2021-12-10 15:37:14 -0300

    requisito 7, mas nao pronto ainda

--- script.js
+++ script.js
@@ -7,14 +7,30 @@ console.log(getList);
 let getInput = document.querySelector('input');
 console.log(getInput.value);
 
-
-
+// função pra adicionar tarefas a lista
 function adicionaTarefa () {
     let createLi = document.createElement('li');
     getList.appendChild(createLi);
     createLi.innerText = getInput.value;
     getInput.value = '';
-
+    addClickList();
 }
 
-getButton.addEventListener('click', adicionaTarefa)
+getButton.addEventListener('click', adicionaTarefa);
+
+
+let getItemList = document.getElementsByTagName('li');
+console.log(getItemList);
+
+function changeBackgroudColor () {
+    for (let i = 0; i < getItemList.length; i += 1) {
+        getItemList[i].style.backgroundColor = 'rgb(128, 128, 128)';
+        break;
+    }
+}
+
+function addClickList () {
+    for (let i = 0; i < getItemList.length; i += 1) {
+    getItemList[i].addEventListener('click', changeBackgroudColor);
+    }
+}
